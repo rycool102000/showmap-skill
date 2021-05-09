1d925988184a9ff6332a7b76522b163c43c3721a
rycool102000 <83560763+rycool102000@users.noreply.github.com>
2021-05-09 16:09:46 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
+import re
+import sys
+import json
+from os.path import dirname, join
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.messagebus.message import Message
+from mycroft.skills.core import resting_screen_handler
 
 
 class Showmap(MycroftSkill):
@@ -8,7 +15,9 @@ class Showmap(MycroftSkill):
     @intent_file_handler('showmap.intent')
     def handle_showmap(self, message):
         self.speak_dialog('showmap')
-
+        self.gui.clear()
+        self.enclosure.display_manager.remove_active()
+        self.gui.show_image("https://www.binghamton.edu/clt/utc/img/new_bartle_map_small.jpg", "School Map", "School Map")
 
 def create_skill():
     return Showmap()
